74eef361412305b53f46b8c68d051f7462b4c95f
Jacob Robinson <Jacobrobinson4321@gmail.com>
2025-08-14 15:58:15 -0400

    Start script

---
+++ AIChatbot/Assets/Scripts/FaceBlend.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FaceBlend : MonoBehaviour
+{
+    // TODO: Create a map of all phenomes to appropriate mouth shapes
+    // Use SentisTTS phenome generator to get all the phenomes in the given text
+    // assume each phenome takes an equal part of the audio to play and have it go along with the elevenlabs generated audio files
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
